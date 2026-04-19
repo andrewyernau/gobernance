20e1e20027718995d65829a18a9e7f0651c1beaa
André Yermak Naumenko <asyermak@gmail.com>
2026-04-19 20:05:36 +0200

    feature: shaders modules

--- games/cubix/src/main.rs
+++ games/cubix/src/main.rs
@@ -22,6 +22,8 @@ use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::window::{Window, WindowBuilder};
 
+use vulkanalia::bytecode::Bytecode;
+
 use vulkanalia::Version;
 use vulkanalia::loader::{LIBRARY, LibloadingLoader};
 use vulkanalia::prelude::v1_0::*;
@@ -222,7 +224,38 @@ impl App {
 }
 
 unsafe fn create_pipeline(device: &Device, data: &mut AppData) -> Result<()> {
+    //STAGES
+
+    let vert = include_bytes!("../shaders/vert.spv");
+    let frag = include_bytes!("../shaders/vert.spv");
+
+    let vert_shader_module = create_shader_module(device, &vert[..])?;
+    let frag_shader_module = create_shader_module(device, &vert[..])?;
+
+    let vert_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::VERTEX)
+        .module(vert_shader_module)
+        .name(b"main\0");
+
+    let frag_stage = vk::PipelineShaderStageCreateInfo::builder()
+        .stage(vk::ShaderStageFlags::FRAGMENT)
+        .module(frag_shader_module)
+        .name(b"main\0");
+
+    // CLEANUP
+
+    device.destroy_shader_module(vert_shader_module, None);
+    device.destroy_shader_module(frag_shader_module, None);
     Ok(())
+}
+
+unsafe fn create_shader_module(device: &Device, bytecode: &[u8]) -> Result<vk::ShaderModule> {
+    let bytecode = Bytecode::new(bytecode).unwrap();
+    let info = vk::ShaderModuleCreateInfo::builder()
+        .code(bytecode.code())
+        .code_size(bytecode.code_size());
+
+    Ok(device.create_shader_module(&info, None)?)
 }
 
 unsafe fn create_swapchain(
